0b33140d243ca1a272c139441e6d31273a21182f
Víctor Andrés Romano <victor.andres.romano@gmail.com>
2024-01-28 13:17:04 -0300

    Pruebas con Routes. Agregado Saludar Controller. App Name ahora es Blog

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hola-mundo', function () {
+    return "¡Hola mundo!";
+});
+
+Route::get('/hola-json', function () {
+    return [1, 2, 3];
+});
+
+Route::get('/home', function () {
+    return response('Esta es nuestra Home', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('Mi-Header-Personalizado', 'Mi Valor Personalizado')
+        ->cookie('hola-cookies', 'mi-valor-encriptado', 60);
+});
+
+Route::redirect('/', '/home');
+
+Route::get('/hola/{persona?}', function ($persona = "humano") {
+    return "Hola " . $persona . "<br><a href='" . route('decir.bienvenido', ["Blogger" => $persona]) . "'>Decir Bienvenido</a>";
+})->where('persona', '[A-Za-z]+')->name('decir.hola');
+
+Route::get('/bienvenido/{blogger?}', function ($blogger = "Blogger") {
+    return "Bienvenido " . $blogger;
+})->where('blogger', '[A-Za-z]+')->name('decir.bienvenido');
